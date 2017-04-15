6896c81bb7f3cc49ada863887c1a3549a4ed7261
Ajay Shibu <ajay@resnova.in>
2017-04-15 07:03:29 +0000

    Basic reporting server and reporting functions added

---
+++ RemoteConnector.py
@@ -0,0 +1,32 @@
+import json
+import urllib
+import datetime
+import threading
+
+REPORT_HTTPS = False
+REPORT_DOMAIN = "whiteovaltechnologies.com"
+REPORT_ENDPOINT = "iParkMobi-Demo-01/updateStatus.php"
+REPORT_PARAM_NAME = "status"
+
+def send_report(CCId, SensorID, OccState):
+    parameterString = json.dumps({
+        "ccid": CCId,
+        "ts": (datetime.datetime.utcnow() - datetime.datetime.utcfromtimestamp(0)).total_seconds(),
+        "d": [{ "sid": SensorID, "eid": 0, "occ": OccState }]
+        })
+    print parameterString
+    queryString = urllib.urlencode({REPORT_PARAM_NAME: parameterString})
+    urlString = "http://"+REPORT_DOMAIN+"/"+REPORT_ENDPOINT+"?"+queryString
+    reader = urllib.urlopen(urlString)
+    responseString = reader.read()
+    print responseString
+    if responseString == "\nSuccess!":
+	return True
+    else:
+        return False
+
+def threaded_send_report(CCId, SensorID, OccState):
+    thread = threading.Thread(target=send_report, args = (CCId, SensorID, OccState))
+    thread.daemon = True
+    thread.start()
+
---
+++ ReportingHelperDaemon.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import struct
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('localhost', 8888)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            recvData = connection.recv(13)
+            dataToUnpack = recvData[4:]
+            ieeeAddress, occupancyState = struct.unpack('QB', dataToUnpack)
+            print "Source IEEE address: %016x" % ieeeAddress
+            if recvData:
+                print >>sys.stderr, 'sending data back to the client'
+                connection.sendall(recvData)
+            else:
+                print >>sys.stderr, 'no more data from', client_address
+                break
+            
+    finally:
+        # Clean up the connection
+        connection.close()
+
+socket.close()
